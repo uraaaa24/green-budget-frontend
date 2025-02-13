0cb081d5c3f091385463eb76f5ff48b3c9300dc0
Yuto Urashima <yuto.urashima24@gmail.com>
2025-02-13 09:30:16 +0900

    feat: 金額セルコンポーネントのスタイルを更新し、フォントの太さを削除

--- src/app/transactions/_components/tables/cells/amount-cell.tsx
+++ src/app/transactions/_components/tables/cells/amount-cell.tsx
@@ -2,7 +2,7 @@ import { TransactionType, TRANSACTION_TYPES } from '@/app/transactions/_schemas/
 
 const ExpenseAmountCell = ({ amount }: { amount: number }) => {
   return (
-    <span className="rounded-full border-red-400 bg-red-100 px-3 py-2 font-bold text-red-800/70">
+    <span className="rounded-full border-red-400 bg-red-100 px-3 py-2 text-red-800/70">
       -{amount}
     </span>
   )
@@ -10,7 +10,7 @@ const ExpenseAmountCell = ({ amount }: { amount: number }) => {
 
 const IncomeAmountCell = ({ amount }: { amount: number }) => {
   return (
-    <span className="rounded-full border-blue-400 bg-blue-100 px-3 py-2 font-bold text-blue-800/70">
+    <span className="rounded-full border-blue-400 bg-blue-100 px-3 py-2 text-blue-800/70">
       {amount}
     </span>
   )
